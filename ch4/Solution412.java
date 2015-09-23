f52d70189939b0b158ec6047fcfebe10a1cd85ab
Yiting Hao <yitinghao.hyt@gmail.com>
2015-09-23 11:22:03 -0400

    4.12 Paths with Sum

--- ch4/Solution412.java
+++ ch4/Solution412.java
@@ -7,6 +7,7 @@ package chapter4;
  * equals to the current running summation minus the target summation. Overall, we do this recursively.
  * Running time complexity: O(n)
  * Space complexity: O(n)
+ * Note: refer to book solution
 */
 import java.util.*;
 public class Solution412 {
@@ -16,29 +17,39 @@ public class Solution412 {
 		Node root = sol412.createTree(values);
 		System.out.println("Print out the binary tree: ");
 		sol412.printOut(root);
+		// three tests, each node is considered a path
 		sol412.printSumResult(5, root);
 		sol412.printSumResult(10, root);
 		sol412.printSumResult(1, root);
 	}
+	// find the number of paths that have summation equals to target summation
 	int pathsWithSum (Node root, int targetSum)
 	{
+		// the hashmap contains the info about running summation, which is the summation from root to current node
 		HashMap<Integer,Integer> numOfRunningSum = new HashMap<>();
+		// initiation the hashmap
 		addToHashmap (numOfRunningSum, 0, 1);
 		return pathsWithSum (root, targetSum, 0, numOfRunningSum);
 	}
+	// overload
 	int pathsWithSum (Node node, int targetSum, int runningSum, HashMap<Integer,Integer> numOfRunningSum)
 	{
 		if (node == null) return 0;
+		// get runningSum for current node and update info in hashmap
 		runningSum += node.val;
 		addToHashmap(numOfRunningSum, runningSum, 1);
 		int numOfPath = 0;
 		if (numOfRunningSum.containsKey(runningSum - targetSum))
 			numOfPath = numOfRunningSum.get(runningSum - targetSum);
+		// recursively revoke function on left subtree
 		numOfPath += pathsWithSum(node.left, targetSum, runningSum, numOfRunningSum);
+		// recursively revoke function on right subtree
 		numOfPath += pathsWithSum(node.right, targetSum, runningSum, numOfRunningSum);
+		// reverse the changes to the hash table so that other nodes don't use it
 		addToHashmap(numOfRunningSum, runningSum, -1);
 		return numOfPath;
 	}
+	// add 
 	void addToHashmap (HashMap<Integer, Integer> map, int key, int change)
 	{
 		if (!map.containsKey(key))
@@ -46,6 +57,7 @@ public class Solution412 {
 		else
 			map.put(key, map.get(key) + change);
 	}
+	// Node class
 	class Node
 	{
 		int val;
@@ -53,6 +65,7 @@ public class Solution412 {
 		Node right;
 		Node(int v) {val = v;}
 	}
+	// create tree function
 	Node createTree (int[] array)
 	{
 		if (array.length == 0) return null;
@@ -77,6 +90,7 @@ public class Solution412 {
 		}
 		return root;
 	}
+	// print out tree layer by layer
 	void printOut (Node root)
 	{
 		if (root == null) return;
@@ -104,6 +118,7 @@ public class Solution412 {
 			System.out.println();
 		}
 	}
+	// print out number of paths that has a summation equals to target summation
 	void printSumResult(int targetSum, Node root)
 	{
 		if (pathsWithSum(root, targetSum) == 0)
