7abae44571870391da0c57c8616af2f01c264088
Yiting Hao <yitinghao.hyt@gmail.com>
2015-09-23 09:31:03 -0400

    4.1 Route Between Nodes

--- ch4/Solution41.java
+++ ch4/Solution41.java
@@ -1,7 +1,8 @@
 package chapter4;
 /* 
  * Algorithm: 
- * Typical breadth first search.
+ * Typical breadth first search. Start from the starting point and do breadth-first search until we find 
+ * the target node.
  * Running time complexity: O(log n)
  * Space complexity: O(n)
 */
@@ -10,22 +11,29 @@ public class Solution41 {
 	public static void main(String[] args) {
 		Solution41 sol41 = new Solution41();
 		int NodeNum = 5;
+		// save routes info as an adjacent list
+		// in this case, the first node points to node 2 and node 4
 		int[][] routes = new int[][] {{2,4},{4},{2},{5},{2}};
 		Graph graph = sol41.new Graph(NodeNum, routes);
-		System.out.println("Route beteween node 1 to 5? " + graph.checkRoute(graph.nodes[0], graph.nodes[4]));
+		sol41.print(0, 4, graph);
+		// need to reset the "visit" value for all nodes
 		graph.reset();
-		System.out.println("Route beteween node 2 to 5? " + graph.checkRoute(graph.nodes[1], graph.nodes[4]));
+		sol41.print(1, 4, graph);
 		graph.reset();
-		System.out.println("Route beteween node 3 to 1? " + graph.checkRoute(graph.nodes[2], graph.nodes[0]));
+		sol41.print(2, 0, graph);
+		graph.reset();
 	}
 	class Graph
 	{
 		Node[] nodes;
+		// constructor, generate graph based on the nodes and edges
 		Graph(int NodeNum, int[][] routes)
 		{
 			nodes = new Node[NodeNum];
+			// build up nodes
 			for (int i = 0; i < NodeNum; i ++)
 				nodes[i] = new Node(i + 1);
+			// establish routes
 			for (int i = 0; i < NodeNum; i ++)
 			{
 				nodes[i].children = new Node[routes[i].length];
@@ -61,11 +69,18 @@ public class Solution41 {
 				nodes[i].visit = false;
 		}
 	}
+	// node class
 	class Node
 	{
 		public int name;
 		public Node[] children;
 		public boolean visit;
+		// constructor
 		Node(int n) {name = n;}
+	}
+	// print result
+	void print (int start, int end, Graph graph)
+	{
+		System.out.println("Route beteween node " + (start + 1) + " to " + (end + 1) +"? "+ graph.checkRoute(graph.nodes[start], graph.nodes[end]));
 	}
 }
