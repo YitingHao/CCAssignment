4296e633bf00cd30136952332ab56e1179ac44dd
Yiting Hao <yitinghao.hyt@gmail.com>
2015-09-23 23:15:51 -0400

    4.2 Minmal Tree

--- ch4/Solution42.java
+++ ch4/Solution42.java
@@ -1,7 +1,8 @@
 package chapter4;
 /* 
+ * Assumption: the given array is sorted with unique integer elements
  * Algorithm: 
- * Do this recursively. Find the mid element to be the root.
+ * Find the mid element to be the root and do this recursively.
  * Running time complexity: O(log n)
  * Space complexity: O(n)
 */
@@ -10,25 +11,26 @@ public class Solution42 {
 	public static void main(String[] args) {
 		Solution42 sol42 = new Solution42();
 		int[] values = new int[] {1, 2, 3, 4, 5, 6, 7, 8};
-		Node binarySearchTree = sol42.createMinimum(values);
+		// create minimal tree
+		Node binarySearchTree = sol42.createMinimum(values, 0, values.length - 1);
 		sol42.printOut(binarySearchTree);
 	}
-	Node createMinimum (int[] array)
-	{
-		return createMinimun (array, 0, array.length - 1);
-	}
-	Node createMinimun (int[] array, int start, int end)
+	// create the minimal tree, take values from an array, define the start and end point (inclusive)
+	Node createMinimum (int[] array, int start, int end)
 	{
 		if (start <= end)
 		{
 			int mid = (end - start + 1) / 2 + start;
+			// set middle element to be the root
 			Node root = new Node (array[mid]);
-			root.leftChild = createMinimun (array, start, mid - 1);
-			root.rightChild = createMinimun (array, mid + 1, end);
+			// recursively build up the tree
+			root.leftChild = createMinimum (array, start, mid - 1);
+			root.rightChild = createMinimum (array, mid + 1, end);
 			return root;
 		}
 		return null;
 	}
+	// function to print out tree layer by layer
 	void printOut (Node root)
 	{
 		if (root == null) return;
@@ -42,9 +44,12 @@ public class Solution42 {
 			height ++;
 			while (!q.isEmpty())
 			{
+				// print out elements in q, which are all in one layer
+				// save these elements in another queue, in order to generate next layer elements
 				System.out.print(q.peek().val + "   ");
 				qTemp.offer(q.poll());
 			}
+			// get elements of next layer and put it back to queue q
 			while (!qTemp.isEmpty())
 			{
 				Node temp = qTemp.poll();
@@ -56,6 +61,7 @@ public class Solution42 {
 			System.out.println();
 		}
 	}
+	// node class
 	class Node
 	{
 		int val;
