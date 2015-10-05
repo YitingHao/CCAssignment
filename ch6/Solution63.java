6ebc71d9e3de2aa590432527a2cf30970a501425
Yiting Hao <yitinghao.hyt@gmail.com>
2015-10-05 16:02:42 -0400

    6.3 Dominos

---
+++ ch6/Solution63.java
@@ -0,0 +1,9 @@
+package chapter6;
+/*  
+ * Explanation: Since each time you put a dominos on the chessboard, the dominos will cover one black and one 
+ * white cell. Therefore, the total number of white cells and black cells should be same if you want to use
+ * dominos to cover all the chessboard. However, in this case, you remove two diagonally opposite corners, which
+ * have the same colors. Therefore, on the chessboard right now, there are 30 black cells and 32 white cells or
+ * 32 black and 30 white. Neither way will give you the same cell number for both colors. Therefore, we can
+ * not fill the chessboard in this case.
+*/
