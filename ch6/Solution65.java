e503fe5dcf39f7c2ed48eb9b809edf97c75d5936
Yiting Hao <yitinghao.hyt@gmail.com>
2015-10-05 21:32:12 -0400

    6.5 Jugs of Water

---
+++ ch6/Solution65.java
@@ -0,0 +1,9 @@
+/* 
+ * Steps:  
+ * 1) fill the five-quart jug;
+ * 2) pour water from five-quart jug to the three-quart (Now there are two quarts of water in the five-quart jug)
+ * 3) empty the three-quart jug and pour all the left water (two quarts) from five-quart jug to three-quart jug
+ * 4) fill the five-quart jug;
+ * 5) pour water from five-quart jug into three-quart jug until it is full
+ * 6) the water left in five-quart jug is four quarts
+*/
